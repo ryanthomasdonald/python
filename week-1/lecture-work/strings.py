bcca189d90d2e4f3466d340141e1485e04996fd5
Ryan Donald <ryanthomasdonald@gmail.com>
2021-09-08 11:03:36 -0400

    made edits

--- week-1/lecture-work/strings.py
+++ week-1/lecture-work/strings.py
@@ -1,1 +1,30 @@
-# learning Python strings
+# strings
+
+"Ryan"
+
+'Donald'
+
+"I'm a new developer."
+
+'I\'ve escaped as a string using a backslash.' # shows how escape characters work (I've)
+
+"""
+We the People of the United States, in Order to form a more perfect Union, establish 
+Justice, insure domestic Tranquility, provide for the common defence, promote the 
+general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do 
+ordain and establish this Constitution for the United States of America.
+"""
+
+print("Hello" + "world.") # Helloworld.
+
+print("3" + "5") # 35 (string)
+
+print(3 + 5) # 8 (integer)
+
+print(3.14 * 2) # 6.28 (integer)
+
+# boolean values have a capitalized first letter
+True # 1
+False # 0
+
+print(True + True) # 2
